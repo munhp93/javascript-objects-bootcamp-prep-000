f71194c09a1431f42093440b70b6378911381b30
Hoang Phuc Nguyen <munhp93@gmail.com>
2018-01-05 21:17:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {artistName,songTitle};
+var playlist = new Object({keys:"0"});
 function updatePlaylist (playlist,artistName,songTitle){
   return playlist.assign({},'Phil Ochs',"Here's to the State of Mississippi");
 }
