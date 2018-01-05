5d038e37839f7d4bc60c31843f405ddcace88e94
Hoang Phuc Nguyen <munhp93@gmail.com>
2018-01-05 21:31:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = playlist({keys:"0"});
+var playlist = new Object({keys:"0"});
 function updatePlaylist (playlist,artistName,songTitle){
-  return playlist.assign({},{[Phil Ochs]:"Here's to the State of Mississippi"});
+  return Object.assign({},{[Phil Ochs]:"Here's to the State of Mississippi"});
 }
